92119e1613d1ec412c4e0f9534c0123840452a49
yatharthasharma <yatharthasharma09@gmail.com>
2023-09-13 01:27:54 +0100

    removed unnecessary complexity from the valid parantheses solution

--- src/com/leetcode/stacks/validparantheses/ValidParantheses.java
+++ src/com/leetcode/stacks/validparantheses/ValidParantheses.java
@@ -4,27 +4,30 @@ import java.util.*;
 
 public class ValidParantheses {
     public static boolean isValid(String s) {
-        Deque<Character> addedOpenChars = new ArrayDeque<>();
+        Stack<Character> addedOpenChars = new Stack<>();
+
         if (s.length() % 2 != 0) {
             return false;
         }
 
         for (char c : s.toCharArray()) {
-            Set<Character> openingChars = new HashSet<>(List.of('[', '{', '('));
-            if (openingChars.contains(c)) {
-                addedOpenChars.push(c);
-            } else {
-                if (c == ']' && Objects.equals(addedOpenChars.peek(), '[')) {
-                    addedOpenChars.pop();
-                } else if (c == '}' && Objects.equals(addedOpenChars.peek(), '{')) {
-                    addedOpenChars.pop();
-                } else if (c == ')' && Objects.equals(addedOpenChars.peek(), '(')) {
-                    addedOpenChars.pop();
-                } else {
+            Map<Character, Character> setsOfChars = new HashMap<>();
+            setsOfChars.put(']', '[');
+            setsOfChars.put('}', '{');
+            setsOfChars.put(')', '(');
+            if (setsOfChars.containsKey(c)) {
+                try {
+                    if(addedOpenChars.peek().equals(setsOfChars.get(c))) {
+                        addedOpenChars.pop();
+                    } else {
+                        return false;
+                    }
+                } catch (EmptyStackException ex) {
                     return false;
                 }
 
-
+            } else {
+                addedOpenChars.push(c);
             }
         }
         return addedOpenChars.isEmpty();
