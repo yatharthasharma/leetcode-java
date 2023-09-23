fc09be387d454cb6888bd96b680dd6c7a56f4b97
yatharthasharma <yatharthasharma09@gmail.com>
2023-09-23 10:01:35 +0100

    add final binary search solution

--- src/com/leetcode/binarysearch/binarysearch/BinarySearch.java
+++ src/com/leetcode/binarysearch/binarysearch/BinarySearch.java
@@ -2,43 +2,36 @@ package com.leetcode.binarysearch.binarysearch;
 
 public class BinarySearch {
     public static int search(int[] nums, int target) {
-        if (nums.length == 1) {
-            if (nums[0] == target) {
-                return 0;
-            }
-            return -1;
-        }
         int p1 = 0;
         int p2 = nums.length - 1;
         while (p1 <= p2) {
-            int index = ((p2 + p1) % 2 == 0) ? (p2 + p1) / 2 : (p2 + p1 + 1) / 2;
-            int checkIndexValue = nums[index];
-            if (checkIndexValue == target) {
+            int index = (p2 + p1) / 2;
+            if (nums[index] == target) {
                 return index;
-            } else if (checkIndexValue > target) {
-                if(p2 == index) {
-                    if(nums[p1] == target) {
-                        return p1;
-                    }
-                    return -1;
+            } else if (nums[index] > target) {
+                if (p2 == index) {
+                    p2--;
+                } else {
+                    p2 = index;
                 }
-                p2 = index;
             } else {
-                if(p1 == index) {
-                    if(nums[p2] == target) {
-                        return p2;
-                    }
-                    return -1;
+                if (p1 == index) {
+                    p1++;
+                } else {
+                    p1 = index;
                 }
-                p1 = index;
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(search(new int[]{-1,0,3,5,9,12}, 2));
-        System.out.println(search(new int[]{-1,0,3,5,9,12}, 9));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 2));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 9));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9}, 9));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9}, 3));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9}, 2));
+        System.out.println(search(new int[]{5}, 5));
     }
 
 }
